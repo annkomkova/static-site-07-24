84480b6d19ea0686d0518f54c6f2228574d464bb
Anna Komkova <ann.komkova98@gmail.com>
2024-12-12 15:44:55 +0300

    build

---
+++ docs/swiper.88d3db6c9e69a6207345.js
@@ -0,0 +1,23 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+var swiper = new Swiper('.swiper', {
+  autoplay: {
+    delay: 5000
+  },
+  effect: 'fade',
+  loop: true,
+  pagination: {
+    el: '.swiper-pagination'
+  },
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev'
+  }
+  // And if we need scrollbar
+  // scrollbar: {
+  //   el: '.swiper-scrollbar'
+  // }
+});
+/******/ })()
+;
